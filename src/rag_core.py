e77ff65da14d88f7dac92c34c82545fa64e13fe6
shubhamlavaniya <shubhamlavaniya77@yahoo.com>
2025-08-23 22:48:37 +0530

    RAG update

--- src/rag_core.py
+++ src/rag_core.py
@@ -2,9 +2,9 @@
 # using LangChain with a hybrid retriever setup.
 
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 import os
